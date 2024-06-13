20b92c5fa3aee353e2a539f04397f280af417abf
sathish1690 <59680830+sathish1690@users.noreply.github.com>
2024-06-13 16:21:55 +0530

    Add files via upload

--- src/app/Shared/homelayout/homelayout.component.ts
+++ src/app/Shared/homelayout/homelayout.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { ServiceComponent } from './service/service.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @Component({
   selector: 'app-homelayout',
   standalone: true,
-  imports: [AboutComponent, ServiceComponent, FeedbackComponent],
+  imports: [AboutComponent, ServiceComponent, FeedbackComponent, ProjectsComponent,PricingComponent],
   templateUrl: './homelayout.component.html',
   styleUrl: './homelayout.component.css'
 })
